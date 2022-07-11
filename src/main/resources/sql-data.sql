59defe714e93c78e4824e8f172bfcf0c4abc22cf
Ian Petts <itisianpetts@gmail.com>
2022-07-11 14:31:44 +0100

    added docs, db connection and schemas

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,2 @@
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO customers (first_name, surname) VALUES ('Ian', 'Petts');
+INSERT INTO items (items_name, items_price) VALUES ('MacBook Air', 999.99);
