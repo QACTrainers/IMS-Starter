d0809a637dc4476d7c1b3533601f139ed0435722
ant <anthonypapasavva@gmail.com>
2022-07-11 15:25:33 +0100

    Added item infrastructure

---
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -0,0 +1,67 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+
+// Here we put 'Item' details for CRUD functionality - like other Controller classes.	
+
+public class ItemController implements CrudController<Item> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private ItemDAO itemDAO;
+	private Utils utils;
+
+	public ItemController(ItemDAO itemDAO, Utils utils) {
+		super();
+		this.itemDAO = itemDAO;
+		this.utils = utils;
+	}
+
+	@Override
+	public List<Item> readAll() {
+		List<Item> items = itemDAO.readAll();
+		for (Item item : items) {
+			LOGGER.info(item.toString());
+		}
+		return items;
+	}
+
+	@Override
+	public Item create() {
+		LOGGER.info("Please enter an Item name");
+		String item_name = utils.getString();
+		LOGGER.info("Please enter the price of the Item");
+		Double item_price = utils.getDouble(); // int or double? can't remember which datatypes Java doesnt like
+		Item item = itemDAO.create(new Item(item_name, item_price));
+		LOGGER.info("Item successfully created");
+		return item;
+	}
+
+	@Override
+	public Item update() {
+		LOGGER.info("Please enter the ID of the item you would like to update");
+		Long item_id = utils.getLong();
+		LOGGER.info("Please enter an Item name");
+		String item_name = utils.getString();
+		LOGGER.info("Please enter a price");
+		Double item_price = utils.getDouble();
+		Item item = itemDAO.update(new Item(item_id, item_name, item_price));
+		LOGGER.info("Item successfully updated");
+		return item;
+	}
+
+	@Override
+	public int delete() {
+		LOGGER.info("Please enter the ID of of the item you would like to delete");
+		Long item_id = utils.getLong();
+		return itemDAO.delete(item_id);
+	}
+
+}
---
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -0,0 +1,68 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.DBUtils;
+
+public class ItemDAO implements Dao<Item> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+		Long item_id = resultSet.getLong("item_id");
+		String item_name = resultSet.getString("item_name");
+		Double item_price = resultSet.getDouble("item_price"); // int or double? can't remember which datatypes Java
+																// doesnt like
+		return new Item(item_id, item_name, item_price);
+	}
+
+	@Override
+	public List<Item> readAll() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM items");){
+			List<Item> items = new ArrayList<>();
+			while (resultSet.next()) {
+				items.add(modelFromResultSet(resultSet));
+			}		
+		return items;
+	} catch (SQLException e){
+
+	@Override
+	public Item create(Item t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item update(Item t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
+}
