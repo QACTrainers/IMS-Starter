871acbdc0350523c06afee7ea4d6f661d1398eb5
ant <anthonypapasavva@gmail.com>
2022-07-11 15:28:18 +0100

    Added Orders Infrastructure

---
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -0,0 +1,21 @@
+package com.qa.ims.persistence.domain;
+
+public class Order {
+
+	private Long orderId;
+	// ? come back to this private long orderItemsId;
+	private Double totalPrice;
+	private int quantity;
+	private Customer customer;
+
+	public Order(Long orderId, )
+	
+}
+
+	public(
+
+	Long orderId, String customer)
+
+	// do I have to instatiate customer and items in order
+
+}
