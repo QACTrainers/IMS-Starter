3452091f67c276eb0a3f061abdf1a9564fd3327b
Ian Petts <itisianpetts@gmail.com>
2022-07-11 14:54:46 +0100

    Add Item domain

---
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -0,0 +1,78 @@
+package com.qa.ims.persistence.domain;
+
+public class Item {
+    private Integer itemId;
+    private String itemName;
+    private double itemPrice;
+
+    public Item(String itemName, double itemPrice) {
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+    }
+
+    public Item(Integer itemId, String itemName, double itemPrice) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(double itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Item ID:" + itemId + ", Item Name:" + itemName + ", Item Price:" + itemPrice + ".";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
+        result = prime * result + ((itemId == null) ? 0 : itemId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Item other = (Item) obj;
+        if (itemName == null) {
+            if (other.itemName != null)
+                return false;
+        } else if (!itemName.equals(other.itemName))
+            return false;
+        if (itemId == null) {
+            if (other.itemId != null)
+                return false;
+        } else if (!itemId.equals(other.itemId))
+            return false;
+        return true;
+    }
+}
