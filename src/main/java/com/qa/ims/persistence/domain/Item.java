67e4e030156715aec2fd48e0db49db2a66d77f7b
Dan Harrison <danhharrison5@gmail.com>
2022-07-11 10:12:06 +0100

    Initial commit of ItemDAO and Item java files

---
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -0,0 +1,40 @@
+package com.qa.ims.persistence.domain;
+
+public class Item {
+
+	private long item_id;
+	private String item_name;
+	private double item_price;
+
+	public Item(long item_id, String item_name, double item_price) {
+		super();
+		this.item_id = item_id;
+		this.item_name = item_name;
+		this.item_price = item_price;
+	}
+
+	public long getItem_id() {
+		return item_id;
+	}
+
+	public void setItem_id(long item_id) {
+		this.item_id = item_id;
+	}
+
+	public String getItem_name() {
+		return item_name;
+	}
+
+	public void setItem_name(String item_name) {
+		this.item_name = item_name;
+	}
+
+	public double getItem_price() {
+		return item_price;
+	}
+
+	public void setItem_price(double item_price) {
+		this.item_price = item_price;
+	}
+
+}
