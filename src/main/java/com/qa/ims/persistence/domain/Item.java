fa508339f95ce742c4129051b5f3ef41507bfa42
S <sarah_dankwa@hotmail.co.uk>
2022-07-12 21:47:50 +0100

    Created Item class

---
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -0,0 +1,103 @@
+package com.qa.ims.persistence.domain;
+
+public class Item {
+
+	private Long itemID;
+	public String itemName;
+	public Double price;
+	public Long stock;
+
+	public Item(String itemName, Double price, Long stock) {
+		this.setItemName(itemName);
+		this.setPrice(price);
+		this.setStock(stock);
+	}
+
+	public Item(Long itemID, String itemName, Double price, Long stock) {
+		this.setItemID(itemID);
+		this.setItemName(itemName);
+		this.setPrice(price);
+		this.setStock(stock);
+	}
+
+	public Long getItemID() {
+		return itemID;
+	}
+
+	public void setItemID(Long itemID) {
+		this.itemID = itemID;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Long getStock() {
+		return stock;
+	}
+
+	public void setStock(Long stock) {
+		this.stock = stock;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [itemID=" + itemID + ", itemName=" + itemName + ", price=" + price + ", stock=" + stock + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((itemID == null) ? 0 : itemID.hashCode());
+		result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		result = prime * result + ((stock == null) ? 0 : stock.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (getItemID() == null) {
+			if (other.getItemID() != null)
+				return false;
+		} else if (!getItemID().equals(other.getItemID()))
+			return false;
+		if (itemName == null) {
+			if (other.itemName != null)
+				return false;
+		} else if (!itemName.equals(other.itemName))
+			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
+		if (stock == null) {
+			if (other.stock != null)
+				return false;
+		} else if (!stock.equals(other.stock))
+			return false;
+		return true;
+	}
+
+}
