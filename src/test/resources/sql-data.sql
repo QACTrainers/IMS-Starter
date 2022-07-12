a4d72ceb66ffa05b8bbf393f3382e2ed64e8473c
feiza15 <feizaahmed159@gmail.com>
2022-07-12 14:44:22 +0100

    i dont know

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,1 +1,2 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `items` (`item_name`, `item_value`, `quantity`, `colour`) VALUES ('butter knife', 2, 25, 'silver');
