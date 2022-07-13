9566a7f4d11d19d7ac1b5bb64030f0b27a45e559
Dan Harrison <danhharrison5@gmail.com>
2022-07-13 10:53:02 +0100

    initial commit of OrderController

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -22,8 +22,10 @@ CREATE TABLE IF NOT EXISTS `items` (
 CREATE TABLE IF NOT EXISTS `orders` (
 	`order_id` INT(8) UNIQUE NOT NULL AUTO_INCREMENT,
 	`fk_customer_id` INT NOT NULL,
+	`fk_item_id` INT NOT NULL,
 	PRIMARY KEY (`order_id`),
-	CONSTRAINT `fk_customer_id` FOREIGN KEY (`fk_customer_id`) REFERENCES customers (`id`)
+	CONSTRAINT `fk_customer_id` FOREIGN KEY (`fk_customer_id`) REFERENCES customers (`id`),
+	CONSTRAINT `fk_item_id` FOREIGN KEY (`fk_item_id`) REFERENCES items (`item_id`)
 	);
 	
 CREATE TABLE IF NOT EXISTS `order_items` (
@@ -32,7 +34,9 @@ CREATE TABLE IF NOT EXISTS `order_items` (
 	`unit_quantity` INT NOT NULL,
 	`fk_order_id` INT NOT NULL,
 	`fk_item_id` INT NOT NULL,
+	`fk_customer_id` INT NOT NULL,
 	PRIMARY KEY (`order_item_id`),
 	CONSTRAINT `fk_order_id` FOREIGN KEY (`fk_order_id`) REFERENCES `orders` (`order_id`),
-	CONSTRAINT `fk_item_id` FOREIGN KEY (`fk_item_id`) REFERENCES `items` (`item_id`)
+	CONSTRAINT `fk_item_id` FOREIGN KEY (`fk_item_id`) REFERENCES `items` (`item_id`),
+	CONSTRAINT `fk_customer_id` FOREIGN KEY (`fk_customer_id`) REFERENCES `customers` (`id`)
 	);
