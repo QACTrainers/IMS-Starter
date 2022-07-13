10352b0e126e2ed08738c15636b5c40bd8f293b7
S <sarah_dankwa@hotmail.co.uk>
2022-07-13 11:47:02 +0100

    Customer class tests at 100% coverage

--- src/main/java/com/qa/ims/persistence/domain/Customer.java
+++ src/main/java/com/qa/ims/persistence/domain/Customer.java
@@ -50,8 +50,8 @@ public class Customer {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
 		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
 		return result;
 	}
@@ -70,10 +70,10 @@ public class Customer {
 				return false;
 		} else if (!getFirstName().equals(other.getFirstName()))
 			return false;
-		if (id == null) {
-			if (other.id != null)
+		if (getId() == null) {
+			if (other.getId() != null)
 				return false;
-		} else if (!id.equals(other.id))
+		} else if (!getId().equals(other.getId()))
 			return false;
 		if (surname == null) {
 			if (other.surname != null)
