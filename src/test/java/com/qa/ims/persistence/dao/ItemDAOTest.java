460637831b7c4b5d4e6a328259cf99693877c8c1
Ian Petts <itisianpetts@gmail.com>
2022-07-13 16:19:08 +0100

    add DAO tests

--- src/test/java/com/qa/ims/persistence/dao/ItemDAOTest.java
+++ src/test/java/com/qa/ims/persistence/dao/ItemDAOTest.java
@@ -1,4 +1,58 @@
 package com.qa.ims.persistence.dao;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.DBUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemDAOTest {
+    private final ItemDAO itemDAO = new ItemDAO();
+
+    @Before
+    public void setup() {
+        DBUtils.connect();
+        DBUtils.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
+    }
+
+    @Test
+    public void createTest() {
+        Item item1 = new Item(1L, "MacBook Air", 699.99);
+        assertEquals(item1, itemDAO.create(item1));
+        assertNull(itemDAO.create(null));
+    }
+
+    @Test
+    public void readLatestTest() {
+        Item item2 =  new Item(1L, "MacBook Air", 699.99);
+        assertEquals(item2, itemDAO.readLatest());
+    }
+
+    @Test
+    public void readTest() {
+        assertEquals(new Item(1L, "MacBook Air", 699.99), itemDAO.read(1L));
+    }
+
+    @Test
+    public void readAllTest() {
+        List<Item> expected = new ArrayList<>();
+        expected.add(new Item(3L, "MacBook Pro", 2500.99));
+        assertEquals(expected, itemDAO.readAll());
+    }
+
+    @Test
+    public void updateTest() {
+        Item item6 = new Item(1L, "MacBook Air", 799.99);
+        assertEquals(item6, itemDAO.update(item6));
+    }
+
+    @Test
+    public void deleteTest() {
+        assertEquals(1, itemDAO.delete(1L));
+    }
 }
--- src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
+++ src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
@@ -1,4 +1,32 @@
 package com.qa.ims.persistence.dao;
 
+import com.qa.ims.persistence.domain.Customer;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.utils.DBUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class OrderDAOTest {
+
+    private final OrderDAO orderDao = new OrderDAO(new CustomerDAO(), new ItemDAO());
+
+    @Before
+    public void setup() {
+        DBUtils.connect();
+        DBUtils.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
+    }
+
+    @Test
+    public void createTest() {
+        Customer customer1 = new Customer(1L, "Ian", "Petts");
+        List<Item> list = new ArrayList<>();
+        Order newOrder = new Order(1L, customer1, 0.0, list);
+        assertEquals(newOrder, orderDao.create(newOrder));
+    }
 }
