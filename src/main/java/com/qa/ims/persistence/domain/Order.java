7ceae83632c052aabbc005a17ee7779da0dda77b
ant <anthonypapasavva@gmail.com>
2022-07-13 15:48:01 +0100

    added order infrastructure

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -3,19 +3,91 @@ package com.qa.ims.persistence.domain;
 public class Order {
 
 	private Long orderId;
-	// ? come back to this private long orderItemsId;
+	// private Long orderItemId; ??
+	private Long id; // ( = customerId)
+	private Long itemId;
 	private Double totalPrice;
-	private int quantity;
-	private Customer customer;
 
-	public Order(Long orderId, )
-	
-}
+	public Order(Long id, Long itemId) {
+		this.id = id;
+		this.itemId = itemId;
+	}
 
-	public(
+	public Order(Long orderId, Long id, Long itemId, Double totalPrice) {
+		super();
+		this.orderId = orderId;
+		this.id = id;
+		this.itemId = itemId;
+		this.totalPrice = totalPrice;
+	}
 
-	Long orderId, String customer)
+	public Long getOrderId() {
+		return orderId;
+	}
 
-	// do I have to instatiate customer and items in order
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(Long itemId) {
+		this.itemId = itemId;
+	}
+
+	public Double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(Double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "Order ID: " + orderId + "Customer ID: " + id + "Item ID: " + itemId + "Total Price: " + totalPrice;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (getOrderId() == null) {
+			if (other.getOrderId() != null)
+				return false;
+		} else if (!getOrderId().equals(other.getOrderId()))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (itemId == null) {
+			if (other.itemId != null)
+				return false;
+		} else if (!itemId.equals(other.itemId))
+			return false;
+		if (totalPrice == null) {
+			if (other.totalPrice != null)
+				return false;
+		} else if (!totalPrice.equals(other.totalPrice))
+			return false;
+		return true;
+	}
 
 }
