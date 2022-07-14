e1c62c39da02f9ad433015f056f360af5dbe64b3
S <sarah_dankwa@hotmail.co.uk>
2022-07-14 10:16:15 +0100

    Created orderline class and dao

---
+++ src/main/java/com/qa/ims/persistence/dao/OrderlineDAO.java
@@ -0,0 +1,113 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Orderline;
+import com.qa.ims.utils.DBUtils;
+
+public class OrderlineDAO implements Dao<Orderline> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public List<Orderline> readAll() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM Orderline");) {
+			List<Orderline> orderlines = new ArrayList<>();
+			if (resultSet.next()) {
+				orderlines.add(modelFromResultSet(resultSet));
+			}
+			return orderlines;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+	}
+
+	public Orderline readLatest() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement
+						.executeQuery("SELECT * FROM Orderline ORDER BY fk_orderID DESC LIMIT 1");) {
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public Orderline read(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Orderline create(Orderline orderline) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("INSERT INTO Orderline VALUES (?, ?, ?)");) {
+			statement.setLong(1, orderline.getFk_orderID());
+			statement.setLong(2, orderline.getFk_itemID());
+			statement.setLong(3, orderline.getQuantity());
+			statement.executeUpdate();
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public Orderline update(Orderline orderline) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("UPDATE Orderline SET fk_itemID = ?, quantity = ? WHERE fk_orderID = ?");) {
+			statement.setLong(1, orderline.getFk_itemID());
+			statement.setLong(2, orderline.getQuantity());
+			statement.setLong(3, orderline.getFk_orderID());
+			statement.executeUpdate();
+			return read(orderline.getFk_orderID());
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public int delete(long fk_orderID) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("DELETE FROM Orderline WHERE fk_orderID = ?");) {
+			statement.setLong(1, fk_orderID);
+			return statement.executeUpdate();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return 0;
+	}
+
+	@Override
+	public Orderline modelFromResultSet(ResultSet resultSet) throws SQLException {
+		Long fk_orderID = resultSet.getLong("fk_orderID");
+		Long fk_itemID = resultSet.getLong("fk_itemID");
+		Long quantity = resultSet.getLong("quantity");
+		return new Orderline(fk_orderID, fk_itemID, quantity);
+	}
+
+}
---
+++ src/main/java/com/qa/ims/persistence/domain/Orderline.java
@@ -0,0 +1,76 @@
+package com.qa.ims.persistence.domain;
+
+public class Orderline {
+
+	private Long fk_orderID;
+	private Long fk_itemID;
+	private Long quantity;
+
+	public Orderline(Long fk_orderID, Long fk_itemID, Long quantity) {
+		this.setFk_orderID(fk_orderID);
+		this.setFk_itemID(fk_itemID);
+		this.setQuantity(quantity);
+	}
+
+	public Long getFk_orderID() {
+		return fk_orderID;
+	}
+
+	public void setFk_orderID(Long fk_orderID) {
+		this.fk_orderID = fk_orderID;
+	}
+
+	public Long getFk_itemID() {
+		return fk_itemID;
+	}
+
+	public void setFk_itemID(Long fk_itemID) {
+		this.fk_itemID = fk_itemID;
+	}
+
+	public Long getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Long quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fk_orderID == null) ? 0 : fk_orderID.hashCode());
+		result = prime * result + ((fk_itemID == null) ? 0 : fk_itemID.hashCode());
+		result = prime * result + ((quantity == null) ? 0 : quantity.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Orderline other = (Orderline) obj;
+		if (getFk_orderID() == null) {
+			if (other.getFk_orderID() != null)
+				return false;
+		} else if (!getFk_orderID().equals(other.getFk_orderID()))
+			return false;
+		if (getFk_itemID() == null) {
+			if (other.getFk_itemID() != null)
+				return false;
+		} else if (!getFk_itemID().equals(other.getFk_itemID()))
+			return false;
+		if (quantity == null) {
+			if (other.quantity != null)
+				return false;
+		} else if (!quantity.equals(other.quantity))
+			return false;
+		return true;
+	}
+
+}
