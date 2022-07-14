d3e02993547ec28f8e44efde78cea3397691ffc2
Dan Harrison <danhharrison5@gmail.com>
2022-07-14 12:08:41 +0100

    orderDAOTest and Schema updates

---
+++ src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
@@ -0,0 +1,60 @@
+package com.qa.ims.persistence.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.utils.DBUtils;
+
+public class OrderDAOTest {
+	private final OrderDAO DAO = new OrderDAO();
+
+	@BeforeClass
+	public static void init() {
+		DBUtils.connect();
+	}
+
+	@Before
+	public void setup() {
+		DBUtils.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
+	}
+
+	@Test
+	public void TestCreate() {
+		final Order created = new Order(3L, 2L);
+		assertEquals(created, DAO.create(new Order(3L, 2L)));
+	}
+
+	@Test
+	public void TestUpdate() {
+		assertEquals(null, DAO.update(new Order(2l, 2l)));
+	}
+
+	@Test
+	public void TestDelete() {
+		assertEquals(1, DAO.delete(1l));
+	}
+
+	@Test
+	public void TestreadSingle() {
+		Order read = new Order(1L, 1L);
+		assertEquals(read, DAO.read(1L));
+	}
+
+	@Test
+	public void TestreadALL() {
+		List<Order> all = new ArrayList<Order>();
+		Order O1 = new Order(1L, 1L);
+		Order O2 = new Order(2L, 2L);
+		all.add(O1);
+		all.add(O2);
+		assertEquals(all, DAO.readAll());
+	}
+
+}
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,4 +1,7 @@
 DROP TABLE IF EXISTS `customers`;
+DROP TABLE IF EXISTS `items`;
+DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `order_items`;
 
 CREATE TABLE IF NOT EXISTS `customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
@@ -6,3 +9,34 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `items` (
+	`item_id` INT(8) NOT NULL AUTO_INCREMENT,
+	`item_name` VARCHAR(40) NULL DEFAULT NULL,
+	`item_price` DOUBLE NULL DEFAULT NULL,
+	PRIMARY KEY (`item_id`)
+	);
+	
+	
+CREATE TABLE IF NOT EXISTS `orders` (
+	`order_id` INT(8) UNIQUE NOT NULL AUTO_INCREMENT,
+	`fk_customer_id` INT NOT NULL,
+	`fk_item_id` INT NOT NULL,
+	PRIMARY KEY (`order_id`),
+	CONSTRAINT `fk_customer_id` FOREIGN KEY (`fk_customer_id`) REFERENCES customers (`id`),
+	CONSTRAINT `fk_item_id` FOREIGN KEY (`fk_item_id`) REFERENCES items (`item_id`)
+	);
+	
+CREATE TABLE IF NOT EXISTS `order_items` (
+	`order_item_id` INT (8) UNIQUE NOT NULL AUTO_INCREMENT,
+	`unit_price` DOUBLE NOT NULL,
+	`item_name` VARCHAR (55) NOT NULL,
+	`unit_quantity` INT NOT NULL DEFAULT 1,
+	`fk_order_id` INT NOT NULL,
+	`fk2_item_id` INT NOT NULL,
+	`fk2_customer_id` INT NOT NULL,
+	PRIMARY KEY (`order_item_id`),
+	CONSTRAINT `fk_order_id` FOREIGN KEY (`fk_order_id`) REFERENCES `orders` (`order_id`),
+	CONSTRAINT `fk2_item_id` FOREIGN KEY (`fk2_item_id`) REFERENCES `items` (`item_id`),
+	CONSTRAINT `fk2_customer_id` FOREIGN KEY (`fk2_customer_id`) REFERENCES `customers` (`id`)
+	);
